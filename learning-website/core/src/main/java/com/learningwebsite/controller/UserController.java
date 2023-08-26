f788d04da0615663bd30d699946c3acfd58b703e
Hao Vo <haovo1512@gmail.com>
2023-08-26 13:14:12 +0700

    fist commit

---
+++ learning-website/core/src/main/java/com/learningwebsite/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.learningwebsite.controller;
+
+import entities.User;
+
+public class UserController {
+    private void test(){
+    }
+}
---
+++ learning-website/models/src/main/java/entities/User.java
@@ -0,0 +1,16 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    String id;
+    String name;
+    String address;
+}
