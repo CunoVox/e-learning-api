2f225792bb1db9bb41962b1344293ed6e0546c4a
thomsonbel12 <thomsonbel12@gmail.com>
2023-09-07 17:35:53 +0700

    ++upcode

--- models/src/main/java/com/elearning/utils/enumAttribute/EnumVerificationCode.java
+++ models/src/main/java/com/elearning/utils/enumAttribute/EnumVerificationCode.java
@@ -2,6 +2,7 @@ package com.elearning.utils.enumAttribute;
 
 public enum EnumVerificationCode {
     EMAIL_CONFIRM,
+    CHANGE_EMAIL_CONFIRM,
     RESET_PASSWORD_CONFIRM,
-    CHANGE_PASSWORD_CONFIRM
+    CHANGE_PASSWORD_CONFIRM,
 }
