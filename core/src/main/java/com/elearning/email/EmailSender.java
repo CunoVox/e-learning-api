5102bcf94fefb70b8f0878f641947082c382dcc6
thomsonbel12 <thomsonbel12@gmail.com>
2023-09-09 12:09:21 +0700

    ++upcode

--- core/src/main/java/com/elearning/email/EmailSender.java
+++ core/src/main/java/com/elearning/email/EmailSender.java
@@ -1,5 +1,9 @@
 package com.elearning.email;
 
+import com.elearning.entities.VerificationCode;
+
 public interface EmailSender {
     void send(String to, String email);
+    void sendMail(String to, VerificationCode code);
+//    void sendUserEmailVerification(String to, VerificationCode code);
 }
--- service/src/main/java/com/elearning/apis/auth/AuthAPI.java
+++ service/src/main/java/com/elearning/apis/auth/AuthAPI.java
@@ -51,12 +51,16 @@ public class AuthAPI {
     }
     @GetMapping("/email-confirm")
     public ResponseEntity<?> emailConfirm(@RequestParam("token") String token) {
-        verificationCodeController.EmailConfirmCode(token);
+        verificationCodeController.emailConfirmCode(token);
         return ResponseEntity.ok().build();
     }
     @GetMapping("/resend-email/{userId}")
     public ResponseEntity<?> resendEmail(@PathVariable("userId") String userId){
-        return ResponseEntity.ok().body(verificationCodeController.reCreateEmailCode(userId));
+        return ResponseEntity.ok().body(verificationCodeController.reCreateEmailConfirmCode(userId));
+    }
+    @GetMapping("/reset-password/{userId}")
+    public ResponseEntity<?> resetPassword(@PathVariable("userId") String userId){
+        return ResponseEntity.ok().body(verificationCodeController.reCreateResetPasswordCode(userId));
     }
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody UserLoginDTO userFormDTO) throws ServiceException {
