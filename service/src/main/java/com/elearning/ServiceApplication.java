fa31af6e28a6c5b7c91aea5de258ac687c678de2
Hao Vo <haovo1512@gmail.com>
2023-11-14 22:30:17 +0700

    ++upcode

--- service/src/main/java/com/elearning/ServiceApplication.java
+++ service/src/main/java/com/elearning/ServiceApplication.java
@@ -18,7 +18,7 @@ public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
         log.info("-----------------------------------------------------------");
-        log.info("🚀 Api doc ready at : " +
+        log.info("🚀 Api doc ready at : " + 
                 Constants.SERVICE_URL + "/swagger-ui/index.html?configUrl=/e-learning/api-docs/swagger-config");
     }
 }
