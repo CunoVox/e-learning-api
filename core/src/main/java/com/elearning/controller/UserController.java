d5d82a5b48d2351f14b65a37c6da193a5bed05aa
thomsonbel12 <thomsonbel12@gmail.com>
2023-11-29 22:58:52 +0700

    ++upcode

--- core/src/main/java/com/elearning/controller/UserController.java
+++ core/src/main/java/com/elearning/controller/UserController.java
@@ -184,19 +184,25 @@ public class UserController extends BaseController {
         if (user.isEmpty()) {
             throw new ServiceException("Vui lòng đăng nhập");
         }
-        if(dto.getPhoneNumber().isBlankOrNull()){
-            throw new ServiceException("Vui lòng nhập số điện thoại");
-        }
+//        if(dto.getPhoneNumber().isBlankOrNull()){
+//            throw new ServiceException("Vui lòng nhập số điện thoại");
+//        }
 //        if(dto.getProfileLink().isBlankOrNull()){
 //            throw new ServiceException("Vui lòng nhập đường dẫn");
 //        }
-        if(dto.getDescription().isBlankOrNull()){
-            throw new ServiceException("Vui lòng mô tả");
-        }
+//        if(dto.getDescription().isBlankOrNull()){
+//            throw new ServiceException("Vui lòng mô tả");
+//        }
         User newUser = user.get();
-        newUser.setPhoneNumber(dto.getPhoneNumber());
-        newUser.setProfileLink(dto.getProfileLink());
-        newUser.setDescription(dto.getDescription());
+        if (!dto.getPhoneNumber().isBlankOrNull()) {
+            newUser.setPhoneNumber(dto.getPhoneNumber());
+        }
+        if (!dto.getProfileLink().isBlankOrNull()) {
+            newUser.setProfileLink(dto.getProfileLink());
+        }
+        if (!dto.getDescription().isBlankOrNull()) {
+            newUser.setDescription(dto.getDescription());
+        }
         if (!newUser.getRoles().contains(EnumRole.ROLE_LECTURE)) {
             newUser.getRoles().add(EnumRole.ROLE_LECTURE);
         }
